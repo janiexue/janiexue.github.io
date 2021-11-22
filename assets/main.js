bd77dd49e4931694dfc37625d3f043e5d025c6c5
Janie Xue <janiexue22@gmail.com>
2021-11-22 17:09:41 -0500

    index and js

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+/* portoflio javascript */
+
+// homepage arrow animation -- horizontal shake //
+function homeOnLoad() {
+    var homeArrow = document.getElementById("home-arrow");
+    homeArrow.addEventListener('mouseover', function (e) {
+        homeArrow.classList.add('animate__animated');
+        homeArrow.classList.add('animate__shakeX');
+    });
+
+    homeArrow.addEventListener('mouseout', function (e) {
+        homeArrow.classList.remove('animate__animated');
+        homeArrow.classList.remove('animate__shakeX');
+    });
+  }
+
